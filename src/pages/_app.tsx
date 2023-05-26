7df455ffdb3e684b73fbfe330e831dcbda6bd10b
Mago Minimalista <contato@magominimalista.com.br>
2023-05-26 04:22:23 -0300

    Concertando erro na página _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
+import { AppProps } from "next/app";
+import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import Head from "next/head";
 
@@ -8,32 +8,30 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { SideNav } from "~/components/SideNav";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+const MyApp = ({ Component, pageProps }: AppProps<{ session: Session | null }>) => {
+  const { session, ...restPageProps } = pageProps;
+
   return (
     <SessionProvider session={session}>
       <Head>
-        <title>Twitter Clone - Apenas para fins de didáticos</title>
-        <meta name="description" content="Clone do Twitter desenvolvido como projeto de estudo e aprendizado. Este site não possui afiliação com o Twitter e não oferece funcionalidades de postagem real ou acesso a dados reais do Twitter.">
-        <meta name="clone-twitter-purpose" content="Estudo e aprendizado do desenvolvimento web">
-        <meta name="keywords" content="magominimalista, twitter-clone">
-        <meta name="author" content="Mago Minimalista">
-        <meta name="robots" content="index, follow">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Twitter Clone</title>
+        <meta
+          name="description"
+          content="Clone do Twitter desenvolvido como projeto de estudo e aprendizado. Este site não possui afiliação com o Twitter e não oferece funcionalidades de postagem real ou acesso a dados reais do Twitter."
+        />
+        <meta name="clone-twitter-purpose" content="Estudo e aprendizado do desenvolvimento web" />
+        <meta name="keywords" content="magominimalista, twitter-clone" />
+        <meta name="author" content="Mago Minimalista" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="bg-gray-900 text-gray-50">
         <div className="container mx-auto flex items-start sm:pr-4">
           <SideNav />
-          <div className="min-h-screen flex-grow border-x border-gray-800">
-            <Component {...pageProps} />
+          <div className="min-h-screen flex-grow border-x">
+            <Component {...restPageProps} />
           </div>
-          
         </div>
       </div>
-      
     </SessionProvider>
   );
 };
