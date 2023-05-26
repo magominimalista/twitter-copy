4d05a3324014f47b679933fe31deb63c7b415d69
Mago Minimalista <contato@magominimalista.com.br>
2023-05-26 06:19:34 -0300

    Corrigindo erro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,37 @@
-import { NextPage } from "next";
+import { type NextPage } from "next";
 import { useSession } from "next-auth/react";
 import { useState } from "react";
 import { InfiniteTweetList } from "~/components/InfiniteTweetList";
 import { NewTweetForm } from "~/components/NewTweetForm";
-import { RecentTweets, FollowingTweets } from "~/components/TweetComponents";
 import { api } from "~/utils/api";
-import TabButton from "~/components/TabButton";
 
 const TABS = ["Recent", "Following"] as const;
 
-type Tab = typeof TABS[number];
-
 const Home: NextPage = () => {
-  const [selectedTab, setSelectedTab] = useState<Tab>("Recent");
+  const [selectedTab, setSelectedTab] =
+    useState<(typeof TABS)[number]>("Recent");
   const session = useSession();
-
-  const handleTabClick = (tab: Tab) => {
-    setSelectedTab(tab);
-  };
-
   return (
     <>
       <header className="sticky top-0 z-10 border-b border-gray-600 bg-gray-900 pt-2">
         <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
         {session.status === "authenticated" && (
           <div className="flex">
-            {TABS.map((tab) => (
-              <TabButton
-                key={tab}
-                tab={tab}
-                selectedTab={selectedTab}
-                onClick={handleTabClick}
-              />
-            ))}
+            {TABS.map((tab) => {
+              return (
+                <button
+                  key={tab}
+                  className={`flex-grow p-2 hover:bg-gray-800 focus-visible:bg-gray-800 ${
+                    tab === selectedTab
+                      ? "border-b-4 border-b-blue-500 font-bold"
+                      : ""
+                  }`}
+                  onClick={() => setSelectedTab(tab)}
+                >
+                  {tab}
+                </button>
+              );
+            })}
           </div>
         )}
       </header>
@@ -41,5 +40,39 @@ const Home: NextPage = () => {
     </>
   );
 };
+
+function RecentTweets() {
+  const tweets = api.tweet.infiniteFeed.useInfiniteQuery(
+    {},
+    { getNextPageParam: (lastPage) => lastPage.nextCursor }
+  );
+
+  return (
+    <InfiniteTweetList
+      tweets={tweets.data?.pages.flatMap((page) => page.tweets)}
+      isError={tweets.isError}
+      isLoading={tweets.isLoading}
+      hasMore={tweets.hasNextPage}
+      fetchNewTweets={tweets.fetchNextPage}
+    />
+  );
+}
+
+function FollowingTweets() {
+  const tweets = api.tweet.infiniteFeed.useInfiniteQuery(
+    { onlyFollowing: true },
+    { getNextPageParam: (lastPage) => lastPage.nextCursor }
+  );
+
+  return (
+    <InfiniteTweetList
+      tweets={tweets.data?.pages.flatMap((page) => page.tweets)}
+      isError={tweets.isError}
+      isLoading={tweets.isLoading}
+      hasMore={tweets.hasNextPage}
+      fetchNewTweets={tweets.fetchNextPage}
+    />
+  );
+}
 
 export default Home;
