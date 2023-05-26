173898f3727c206077bfe50e5fbd360c56ca7f4c
Mago Minimalista <contato@magominimalista.com.br>
2023-05-26 06:05:32 -0300

    Correção de erro crítico

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -1,14 +1,28 @@
-import React from "react";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
 import InfiniteScroll from "react-infinite-scroll-component";
+import { ProfileImage } from "./ProfileImage";
+import { VscHeart, VscHeartFilled } from "react-icons/vsc";
+import { IconHoverEffect } from "./IconHoverEffect";
+import { api } from "~/utils/api";
 import { LoadingSpinner } from "./LoadingSpinner";
-import { TweetCard, Tweet } from "./InfiniteTweetCard";
+import { formatDateTime } from "./DateTimeUtils";
+
+type Tweet = {
+  id: string;
+  content: string;
+  createdAt: Date;
+  likeCount: number;
+  likedByMe: boolean;
+  user: { id: string; image: string | null; name: string | null };
+};
 
 type InfiniteTweetListProps = {
-  tweets?: Tweet[];
-  isError: boolean;
   isLoading: boolean;
-  fetchNewTweets: () => Promise<unknown>;
+  isError: boolean;
   hasMore: boolean | undefined;
+  fetchNewTweets: () => Promise<unknown>;
+  tweets?: Tweet[];
 };
 
 export function InfiniteTweetList({
@@ -40,5 +54,139 @@ export function InfiniteTweetList({
         })}
       </InfiniteScroll>
     </ul>
+  );
+}
+
+function TweetCard({
+  id,
+  user,
+  content,
+  createdAt,
+  likeCount,
+  likedByMe,
+}: Tweet) {
+  const trpcUtils = api.useContext();
+  const toggleLike = api.tweet.toggleLike.useMutation({
+    onSuccess: ({ addedLike }) => {
+      const updateData: Parameters<
+        typeof trpcUtils.tweet.infiniteFeed.setInfiniteData
+      >[1] = (oldData) => {
+        if (oldData == null) return;
+
+        const countModifier = addedLike ? 1 : -1;
+
+        return {
+          ...oldData,
+          pages: oldData.pages.map((page) => {
+            return {
+              ...page,
+              tweets: page.tweets.map((tweet) => {
+                if (tweet.id === id) {
+                  return {
+                    ...tweet,
+                    likeCount: tweet.likeCount + countModifier,
+                    likedByMe: addedLike,
+                  };
+                }
+
+                return tweet;
+              }),
+            };
+          }),
+        };
+      };
+
+      trpcUtils.tweet.infiniteFeed.setInfiniteData({}, updateData);
+      trpcUtils.tweet.infiniteFeed.setInfiniteData(
+        { onlyFollowing: true },
+        updateData
+      );
+      trpcUtils.tweet.infiniteProfileFeed.setInfiniteData(
+        { userId: user.id },
+        updateData
+      );
+    },
+  });
+
+  function handleToggleLike() {
+    toggleLike.mutate({ id });
+  }
+
+  return (
+    <li className="flex gap-4 border-b border-gray-800 px-4 py-4">
+      <Link href={`/profile/${user.id}`}>
+        <ProfileImage src={user.image} />
+      </Link>
+      <div className="flex flex-grow flex-col">
+        <div className="flex gap-1">
+          <Link
+            href={`/profile/${user.id}`}
+            className="font-bold outline-none hover:underline focus-visible:underline"
+          >
+            {user.name}
+          </Link>
+          <span className="text-gray-500">-</span>
+          <span className="text-gray-500">
+            {formatDateTime(createdAt)}
+          </span>
+        </div>
+        <p className="whitespace-pre-wrap">{content}</p>
+        <HeartButton
+          onClick={handleToggleLike}
+          isLoading={toggleLike.isLoading}
+          likedByMe={likedByMe}
+          likeCount={likeCount}
+        />
+      </div>
+    </li>
+  );
+}
+
+type HeartButtonProps = {
+  onClick: () => void;
+  isLoading: boolean;
+  likedByMe: boolean;
+  likeCount: number;
+};
+
+function HeartButton({
+  isLoading,
+  onClick,
+  likedByMe,
+  likeCount,
+}: HeartButtonProps) {
+  const session = useSession();
+  const HeartIcon = likedByMe ? VscHeartFilled : VscHeart;
+
+  if (session.status !== "authenticated") {
+    return (
+      <div className="mb-1 mt-1 flex items-center gap-3 self-start text-gray-500">
+        <HeartIcon />
+        <span>{likeCount}</span>
+      </div>
+    );
+  }
+
+  return (
+    <button
+      disabled={isLoading}
+      onClick={onClick}
+      className={`group -ml-2 flex items-center gap-1 self-start transition-colors duration-200 ${
+        likedByMe
+          ? "text-red-500"
+          : "text-gray-500 hover:text-red-500 focus-visible:text-red-500"
+      }`}
+    >
+      <IconHoverEffect red>
+        <HeartIcon
+          className={`transition-colors duration-200 ${
+            likedByMe
+              ? "fill-red-500"
+              : "fill-gray-500 group-hover:fill-red-500 group-focus-visible:fill-red-500"
+          }`}
+        />
+      </IconHoverEffect>
+      <span>{likeCount}</span>
+    </button>
   );
 }
